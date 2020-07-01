a0b38f9c9252806fa7ff76c62c56fb9bb173117e
lucymait <lucy.maitland@hotmail.com>
2020-07-01 16:33:35 +0100

    changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,39 +22,39 @@ class Search extends React.Component {
     }, 1000)
   }
   render() {
-    if (!this.state.data.track)
-      return <Spinner
-      />
-    else
-      return <section className="section">
-        <div className="container">
-          <div className="columns is-mobile is-multiline">
-            {this.state.data.map((track, key) => {
-              return <div key={key} className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-                <div className="card">
-                  <div className="album-cover">
-                    <figure className="image is-4by3">
-                      <img src={track.album.cover_medium} alt={track.artist.name} />
-                    </figure>
-                    {track.data}
-                  </div>
-                  <div className="card-artist-name">
-                    <Link to={`/artist/${track.artist.id}`}>{track.artist.name}</Link>
-                  </div>
-                  <div className="card-content">
-                    <p>{track.title}</p>
-                  </div>
-                  <audio className="playPreview"
-                    src={track.preview}
-                    controls
-                  />
+    // if (!this.state.data.track)
+    //   return <Spinner
+    //   />
+    // else
+    return <section className="section">
+      <div className="container">
+        <div className="columns is-mobile is-multiline">
+          {this.state.data.map((track, key) => {
+            return <div key={key} className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+              <div className="card">
+                <div className="album-cover">
+                  <figure className="image is-4by3">
+                    <img src={track.album.cover_medium} alt={track.artist.name} />
+                  </figure>
+                  {track.data}
                 </div>
+                <div className="card-artist-name">
+                  <Link to={`/artist/${track.artist.id}`}>{track.artist.name}</Link>
+                </div>
+                <div className="card-content">
+                  <p>{track.title}</p>
+                </div>
+                <audio className="playPreview"
+                  src={track.preview}
+                  controls
+                />
               </div>
-            })
-            }
-          </div>
+            </div>
+          })
+          }
         </div>
-      </section>
+      </div>
+    </section>
   }
 }
 
